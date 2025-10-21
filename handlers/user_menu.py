d6cc49ece193d4790b78160cc2a7926622e5ab77
lostyawolfer <lostyawolfer@gmail.com>
2025-10-21 02:57:56 +0200

    test

--- handlers/user_menu.py
+++ handlers/user_menu.py
@@ -79,16 +79,17 @@ async def process_audio(audio_filepath, title, artist, thumbnail_url):
 
         # Crop to centered square
         width, height = img.size
+        height *= 0.24
         min_dimension = min(width, height)
 
         # Calculate crop coordinates for centered square
-        # left = (width - min_dimension) // 2
-        # top = (height - min_dimension) // 2
-        # right = left + min_dimension
-        # bottom = top + min_dimension
-        #
-        # # Crop to square
-        # img = img.crop((left, top, right, bottom))
+        left = (width - min_dimension) // 2
+        top = (height - min_dimension) // 2
+        right = left + min_dimension
+        bottom = top + min_dimension
+
+        # Crop to square
+        img = img.crop((left, top, right, bottom))
 
         thumbnail_bytes = BytesIO()
         img.save(thumbnail_bytes, format="JPEG")
